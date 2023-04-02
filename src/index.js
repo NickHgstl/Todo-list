0e8964af738681fa4d2e9c2c1bd15bf72d5ab7b5
NickHgstl <nickhoogst@gmail.com>
2023-04-02 15:10:11 +0200

    added sidebar and header

--- src/index.js
+++ src/index.js
@@ -7,10 +7,24 @@ let bodyContainer = document.createElement('div');
 bodyContainer.id = "body_container";
 div.append(bodyContainer);
 
-function sidebar(scheduled, today, week) {
+function sidebar(scheduled, today, week, newProject) {
     this.scheduled = scheduled;
     this.today = today;
     this.week = week;
+    this.newProject = newProject;
+
+}
+
+
+function header() {
+    let headerContainer = document.createElement("div");
+    headerContainer.id = "header_container";
+    div.append(headerContainer);
+
+    let headerContent = document.createElement("div");
+    headerContent.className = "header_item";
+    headerContainer.innerText = "todo list";
+    headerContainer.append(headerContent);
 }
 
 function pageSidebar () {
@@ -29,8 +43,54 @@ function pageSidebar () {
     sidebar.today.className = "sidebar_item";
     sidebar.today.innerText = "today";
     sidebarContainer.append(sidebar.today);
-    
 
+    sidebar.week = document.createElement("div");
+    sidebar.week.id = "week";
+    sidebar.week.className = "sidebar_item";
+    sidebar.week.innerText = "This week";
+    sidebarContainer.append(sidebar.week);   
+    
+    let sidebarNewProject = document.createElement("div");
+    sidebarNewProject.id = "week";
+    sidebarNewProject.className = "sidebar_item";
+    sidebarNewProject.innerText = "New project";
+    sidebarContainer.append(sidebarNewProject);
+    
+    
+    sidebarNewProject.addEventListener('click', function(e){
+            addCustonProject();
+
+
+        }
+    )
+    function addCustonProject() {
+            let projectInput = document.createElement("input");
+            sidebarContainer.append(projectInput);
+
+            let projectButton = document.createElement("button");
+            projectButton.innerHTML = "confirm";
+            sidebarContainer.append(projectButton);
+
+
+            projectButton.addEventListener('click', function(e){
+                let customProject = document.createElement('div');
+                sidebarContainer.append(customProject);
+                customProject.innerText = projectInput.value;
+                console.log("hey");
+
+                if (projectButton.parentNode)
+                projectButton.parentNode.removeChild(projectButton);
+
+                if (projectInput.parentNode)
+                projectInput.parentNode.removeChild(projectInput);
+
+
+                
+            })
+
+        }
     
 }
+
+header();
 pageSidebar();
