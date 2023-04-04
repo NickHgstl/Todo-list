63919bd4cf4f7aaf059bc0121e0444c9810a737e
NickHgstl <nickhoogst@gmail.com>
2023-04-04 15:22:07 +0200

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ function sidebar(scheduled, today, week, newProject) {
 
 }
 
-
 function header() {
     let headerContainer = document.createElement("div");
     headerContainer.id = "header_container";
@@ -32,65 +31,64 @@ function pageSidebar () {
     sidebarContainer.id = "sidebar_container";
     bodyContainer.append(sidebarContainer);
 
-    sidebar.scheduled = document.createElement("div");
-    sidebar.scheduled.id = "scheduled";
-    sidebar.scheduled.className = "sidebar_item";
-    sidebar.scheduled.innerText = "scheduled";
-    sidebarContainer.append(sidebar.scheduled);
+    const scheduled = document.createElement("div");
+    scheduled.id = "scheduled";
+    scheduled.className = "sidebar_item";
+    scheduled.innerText = "scheduled";
+    sidebar.scheduled = scheduled.innerText;
+    console.log("sidebar item:", sidebar.scheduled);
+    sidebarContainer.append(scheduled);
 
-    sidebar.today = document.createElement("div");
-    sidebar.today.id = "today";
-    sidebar.today.className = "sidebar_item";
-    sidebar.today.innerText = "today";
-    sidebarContainer.append(sidebar.today);
+    const today = document.createElement("div");
+    today.id = "today";
+    today.className = "sidebar_item";
+    today.innerText = "today";
+    sidebar.today = today.innerText;
+    console.log("sidebar item:", sidebar.today);
+    sidebarContainer.append(today);
 
-    sidebar.week = document.createElement("div");
-    sidebar.week.id = "week";
-    sidebar.week.className = "sidebar_item";
-    sidebar.week.innerText = "This week";
-    sidebarContainer.append(sidebar.week);   
+    const week = document.createElement("div");
+    week.id = "week";
+    week.className = "sidebar_item";
+    week.innerText = "This week";
+    sidebar.week = week.innerText;
+    console.log("sidebar item:", sidebar.week);
+    sidebarContainer.append(week);   
     
     let sidebarNewProject = document.createElement("div");
     sidebarNewProject.id = "week";
     sidebarNewProject.className = "sidebar_item";
     sidebarNewProject.innerText = "New project";
-    sidebarContainer.append(sidebarNewProject);
-    
+    sidebar.sidebarNewProject = sidebarNewProject.innerText;
+    console.log("sidebar item:", sidebar.newProject);
+    sidebarContainer.append(sidebarNewProject);    
     
     sidebarNewProject.addEventListener('click', function(e){
-            addCustonProject();
+        let projectInput = document.createElement("input");
+        sidebarContainer.append(projectInput);
+
+        let projectButton = document.createElement("button");
+        projectButton.innerHTML = "confirm";
+        sidebarContainer.append(projectButton);    
 
 
-        }
-    )
-    function addCustonProject() {
-            let projectInput = document.createElement("input");
-            sidebarContainer.append(projectInput);
+        projectButton.addEventListener('click', function(e){
+            let customProject = document.createElement('div');
+            sidebarContainer.append(customProject);
+            customProject.innerText = projectInput.value;
+            sidebar.newProject = customProject.innerText;
+            console.log("new project:",sidebar.newProject);
+            console.log("hey");
 
-            let projectButton = document.createElement("button");
-            projectButton.innerHTML = "confirm";
-            sidebarContainer.append(projectButton);
+            if (projectButton.parentNode)
+            projectButton.parentNode.removeChild(projectButton);
 
-
-            projectButton.addEventListener('click', function(e){
-                let customProject = document.createElement('div');
-                sidebarContainer.append(customProject);
-                customProject.innerText = projectInput.value;
-                console.log("hey");
-
-                if (projectButton.parentNode)
-                projectButton.parentNode.removeChild(projectButton);
-
-                if (projectInput.parentNode)
-                projectInput.parentNode.removeChild(projectInput);
-
-
-                
-            })
-
-        }
-    
+            if (projectInput.parentNode)
+            projectInput.parentNode.removeChild(projectInput);                
+        })
+    })
 }
+
 
 header();
 pageSidebar();
